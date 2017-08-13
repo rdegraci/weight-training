93ec377fadb249f35bacdb413e72d6b37e9b29e9
Rodney Degracia <rdegraci@elvenlabs.com>
2017-08-12 23:11:30 -0400

    Add Database interface class to Project

---
+++ WeightTraining/Database.h
@@ -0,0 +1,13 @@
+//
+//  Database.h
+//  WeightTraining
+//
+//  Created by Rodney Degracia on 8/12/17.
+//  Copyright © 2017 Rodney Degracia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Database : NSObject
+
+@end
---
+++ WeightTraining/Database.m
@@ -0,0 +1,13 @@
+//
+//  Database.m
+//  WeightTraining
+//
+//  Created by Rodney Degracia on 8/12/17.
+//  Copyright © 2017 Rodney Degracia. All rights reserved.
+//
+
+#import "Database.h"
+
+@implementation Database
+
+@end
